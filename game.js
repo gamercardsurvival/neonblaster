213e6579e62ad2e4c2431bb3d22fee3f8e8a832e
gamercardsurvival <gamercardsurvival@gmail.com>
2024-10-30 01:21:59 +0530

    Update game.js

--- game.js
+++ game.js
@@ -21,31 +21,44 @@ const game = new Phaser.Game(config);
 
 // Preload assets
 function preload() {
-  this.load.image('background', 'assets/images/background.png');
-  this.load.image('ship', 'assets/images/ship.png');
-  this.load.image('enemy', 'assets/images/enemy.png');
-  this.load.image('bullet', 'assets/images/bullet.png');
 }
 
 // Create game objects
 function create() {
   // Background
-  this.add.image(400, 300, 'background');
-  
+  this.add.rectangle(400, 300, 800, 600, 0x000000);
+
   // Ship
-  this.ship = this.physics.add.image(400, 500, 'ship');
-  this.ship.setCollideWorldBounds(true);
-  
+  this.ship = this.add.rectangle(400, 500, 50, 50, 0x00FFFF);
+
   // Enemies
-  this.enemies = this.physics.add.group();
-  
+  this.enemies = this.add.group();
+  this.enemies.createMultiple({
+    classType: Phaser.GameObjects.Rectangle,
+    frameConfig: {
+      width: 50,
+      height: 50,
+      fillStyle: { color: 0xFF0000 }
+    },
+    quantity: 10
+  });
+
   // Bullets
-  this.bullets = this.physics.add.group();
-  
+  this.bullets = this.add.group();
+  this.bullets.createMultiple({
+    classType: Phaser.GameObjects.Rectangle,
+    frameConfig: {
+      width: 10,
+      height: 10,
+      fillStyle: { color: 0xFFFF00 }
+    },
+    quantity: 20
+  });
+
   // Score
   this.score = 0;
   this.scoreText = this.add.text(10, 10, 'Score: 0', { fontSize: 32, color: '#ffffff' });
-  
+
   // Input
   this.cursors = this.input.keyboard.createCursorKeys();
 }
@@ -54,13 +67,11 @@ function create() {
 function update(time, delta) {
   // Ship movement
   if (this.cursors.left.isDown) {
-    this.ship.setVelocityX(-200);
+    this.ship.x -= 5;
   } else if (this.cursors.right.isDown) {
-    this.ship.setVelocityX(200);
-  } else {
-    this.ship.setVelocityX(0);
+    this.ship.x += 5;
   }
-  
+
   // Shoot bullet
   if (this.cursors.space.isDown) {
     const bullet = this.bullets.get();
@@ -72,7 +83,7 @@ function update(time, delta) {
         .setVisible(true);
     }
   }
-  
+
   // Enemy spawn
   if (Math.random() < 0.1) {
     const enemy = this.enemies.get();
@@ -84,19 +95,19 @@ function update(time, delta) {
         .setVisible(true);
     }
   }
-  
+
   // Collision detection
   this.physics.collide(this.ship, this.enemies, (ship, enemy) => {
     // Game over logic
     this.scene.pause();
     this.add.text(400, 300, 'Game Over', { fontSize: 64, color: '#ffffff' });
   });
-  
+
   this.physics.collide(this.bullets, this.enemies, (bullet, enemy) => {
     // Increase score
     this.score++;
     this.scoreText.setText(`Score: ${this.score}`);
-    
+
     // Destroy bullet and enemy
     bullet.destroy();
     enemy.destroy();
